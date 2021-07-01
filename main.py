191fb63d3155d19beedcd0b73ea07517e4f3aa71
L3g3nDAr <erik.vasilenko2002@gmail.com>
2021-07-01 23:19:45 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,74 @@
+import sqlite3
+
+
+
+def menu_user():
+    conn = sqlite3.connect('orders.db')
+    cur = conn.cursor()
+    while True:
+        print("""\n1) Check all accounts
+2)Delete account
+3)Logout""")
+        user_choise = int(input("You choose: "))
+        if user_choise == 1:
+            cur.execute("SELECT * FROM users;")
+            all_results = cur.fetchall()
+            print("----------------")
+            for i in all_results:
+                print(i)
+            print("______________")
+        elif user_choise == 2:
+            deleted_acc = input("Enter login of account, that you need to delete: ")
+            sql_update_query = """DELETE from users where login = ?"""
+            cur.execute(sql_update_query, (deleted_acc, ))
+            conn.commit()
+        elif user_choise == 3:
+            menu()
+
+
+def menu():
+    conn = sqlite3.connect('orders.db')
+    cur = conn.cursor()
+    cur.execute("""CREATE TABLE IF NOT EXISTS users(
+    login TEXT,
+    pass TEXT);
+    """)
+    conn.commit()
+    print("""\n1)Log in to the account
+2)Register new account
+3) Exit""")
+    user_choice = int(input("You choose: "))
+    if user_choice == 2:
+        while True:
+            login = input("\nEnter your login: ")
+            password = input("Enter password: ")
+            new_user = (login, password)
+            password_check = input("Enter password secondary: ")
+            if password == password_check:
+                cur.execute("INSERT INTO users VALUES(?, ?);", new_user)
+                conn.commit()
+                print("\nYour registered new account")
+                menu()
+            elif password != password_check:
+                print("\nPasswords not the same, please enter again")
+    elif user_choice == 1:
+        while True:
+            login = input("\nEnter your login: ")
+            password = input("Enter password: ")
+            user = (login, password)
+            cur.execute("SELECT * FROM users;")
+            all_results = cur.fetchall()
+            if user in all_results:
+                print("""------------  
+You entered :)
+-------------""")
+                menu_user()
+            else:
+                print("Login or password not true")
+
+    elif user_choice == 3:
+        print("\nBye!!")
+        quit()
+
+
+menu()
